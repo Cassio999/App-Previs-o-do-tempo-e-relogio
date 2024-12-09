f1e48b623880ead4b3031bb88ccdf343a25ccf46
Cassio Oliveira <139669469+Cassio999@users.noreply.github.com>
2024-12-09 02:55:05 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,82 @@
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr <10) hr = '0' + hr;
+    if (min <10) min = '0' + min;
+    if (s <10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+})
+
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+
+/*O codigo acima em js e referente ao relogio */
+
+document.querySelector('#search').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const cityName = document.querySelector('#city_name').value;
+
+    if (!cityName) {
+        document.querySelector("#weather").classList.remove('show');
+        showAlert('Você precisa digitar uma cidade...');
+        return;
+    }
+
+    const apiKey = '8a60b2de14f7a17c7a11706b2cfcd87c';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+
+    if (json.cod === 200) {
+        showInfo({
+            city: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.temp_min,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
+            windSpeed: json.wind.speed,
+            humidity: json.main.humidity,
+        });
+    } else {
+        document.querySelector("#weather").classList.remove('show');
+        showAlert(`
+            Não foi possível localizar...
+
+            <img src="src/images/404.svg"/>
+        `)
+    }
+});
+
+function showInfo(json){
+    showAlert('');
+
+    document.querySelector("#weather").classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
+
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_description').innerHTML = `${json.description}`;
+    document.querySelector('#temp_img').setAttribute('src', `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+    document.querySelector('#humidity').innerHTML = `${json.humidity}%`;
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)}km/h`;
+}
+
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
+}
